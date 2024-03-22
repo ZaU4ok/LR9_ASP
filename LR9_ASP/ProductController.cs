26a0f60214bf4e3f47dae361849efb789d087fc1
LybitelKoda <92180451+ZaU4ok@users.noreply.github.com>
2024-03-22 19:15:13 +0200

    upload project

---
+++ LR9_ASP/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LR9_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LR9_ASP
+{
+    public class ProductController : Controller
+    {
+        public IActionResult DisplayProducts()
+        {
+            var products = new List<Product>
+        {
+            new Product { ID = 1, Name = "Product 1", Price = 10.99m },
+            new Product { ID = 2, Name = "Product 2", Price = 20.49m },
+            new Product { ID = 3, Name = "Product 3", Price = 15.75m }
+        };
+
+            return View("ProductsView", products);
+        }
+    }
+}
---
+++ LR9_ASP/WeatherComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LR9_ASP
+{
+    public class WeatherComponent : ViewComponent
+    {
+        private readonly string _apiKey = "не знайшов api";
+
+        public async Task<IViewComponentResult> InvokeAsync(string region)
+        {
+            var weatherData = await GetWeatherAsync(region);
+            return View(weatherData);
+        }
+
+        private async Task<WeatherData> GetWeatherAsync(string region)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={region}&appid={_apiKey}&units=metric");
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+                var weatherData = JsonSerializer.Deserialize<WeatherData>(json);
+                return weatherData;
+            }
+        }
+    }
+
+    public class WeatherData
+    {
+        public MainData Main { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class MainData
+    {
+        public double Temp { get; set; }
+    }
+}
